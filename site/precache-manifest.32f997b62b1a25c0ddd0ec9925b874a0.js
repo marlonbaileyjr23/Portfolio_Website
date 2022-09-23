3dbf801337824b5afeeeb25f0ad80cc9f455d44a
Marlon Bailey Jr <108299124+Sweatlys@users.noreply.github.com>
2022-09-22 22:33:22 -0400

    publish

---
+++ site/precache-manifest.32f997b62b1a25c0ddd0ec9925b874a0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a54168306ba2fb36dfc",
+    "url": "/static/css/main.a0666989.chunk.css"
+  },
+  {
+    "revision": "8a54168306ba2fb36dfc",
+    "url": "/static/js/main.8a541683.chunk.js"
+  },
+  {
+    "revision": "9bf32f55384023627f7f",
+    "url": "/static/js/1.9bf32f55.chunk.js"
+  },
+  {
+    "revision": "b8fb6f09db5203a12eaf",
+    "url": "/static/css/2.7c42876b.chunk.css"
+  },
+  {
+    "revision": "b8fb6f09db5203a12eaf",
+    "url": "/static/js/2.b8fb6f09.chunk.js"
+  },
+  {
+    "revision": "9b9f0366207aafdb65e7",
+    "url": "/static/js/runtime~main.9b9f0366.js"
+  },
+  {
+    "revision": "70af9240bb95dd2d65f6bf9f09591507",
+    "url": "/static/media/project-img1.70af9240.png"
+  },
+  {
+    "revision": "98b3bc237950d6cb1a1e49f75372ae36",
+    "url": "/static/media/project-img2.98b3bc23.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "e90d04dc3a817639f5b7a71b67d2b161",
+    "url": "/static/media/MB-logo.e90d04dc.png"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "02181f31944bd05b110ed7297dd0a33f",
+    "url": "/static/media/Resume_pdf.02181f31.pdf"
+  },
+  {
+    "revision": "822ecab0cf7b638d8a38d1722af2016b",
+    "url": "/static/media/header-img.822ecab0.png"
+  },
+  {
+    "revision": "f52f89f44f0fe3dd06b3cbf869bb35f4",
+    "url": "/static/media/contact-img1.f52f89f4.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "66b57e8d4fe5d396d8200c17b875071e",
+    "url": "/static/media/banner-test.66b57e8d.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "28a87fe35bddbdf31a9a3f0a89a2512f",
+    "url": "/index.html"
+  }
+];
